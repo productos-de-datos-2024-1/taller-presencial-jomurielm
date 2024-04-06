b336a5e31e035fc7b2c30bc99d6654d88a56dfd5
jomurielm <144398722+jomurielm@users.noreply.github.com>
2024-04-06 12:54:57 -0500

    Actualización API

--- src/6a_api_client/autograding.py
+++ src/6a_api_client/autograding.py
@@ -3,8 +3,8 @@
 import os
 
 # test code files
-assert os.path.exists("src/6b_api_client/config.json")
-assert os.path.exists("src/6b_api_client/app.py")
+assert os.path.exists("src/6a_api_client/config.json")
+assert os.path.exists("src/6a_api_client/app.py")
 
 
 # test run
--- src/6b_api_server/autograding.py
+++ src/6b_api_server/autograding.py
@@ -3,8 +3,8 @@
 import os
 
 # test code files
-assert os.path.exists("src/6a_api_server/config.json")
-assert os.path.exists("src/6a_api_server/server.py")
+assert os.path.exists("src/6b_api_server/config.json")
+assert os.path.exists("src/6b_api_server/server.py")
 
 
 # test run
